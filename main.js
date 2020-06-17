420ba3f368c504c39f1517c3133ff1d650349dd1
Sofiia Pidhaina <sofiiapidhaina1@gmail.com>
2020-06-17 03:43:21 +0300

    add a JS

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ const age = 16;
 console.log ('Cześć, witaj na mojej stronie!');
 
 console.log (`Mam na imię ${name} i mam ${age} lat`);
+const empertyParagraf = document.querySelector('.notes--js');
+
+empertyParagraf.innerHTML = 'uzupelniam tresc w javascript';
